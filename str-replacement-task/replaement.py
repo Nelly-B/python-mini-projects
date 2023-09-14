c8bb8939b959a62e5eb17a94bec3404572fee2a4
Nelly-B <alapanelly@gmail.com>
2023-09-14 11:25:01 +0100

    str replacement

---
+++ str-replacement-task/replaement.py
@@ -0,0 +1,6 @@
+def replace():
+        text = " God is all it takes to get to the height in life that no man can hi hi hi hi hi"
+        word_to_replace = input('enter the word to replace: ')
+        word_to_replace_with = input('enter the word to replace with: ')
+        print(text.replace(word_to_replace, word_to_replace_with)) 
+replace()
